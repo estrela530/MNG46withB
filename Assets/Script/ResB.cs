5060a4806db02af76ad0ab2f9372ba014f25e216
estrela530 <daisuke.0530.otk@gmail.com>
2021-03-22 13:59:43 +0900

    ボタン改善（失敗）

--- Assets/Script/ResB.cs
+++ Assets/Script/ResB.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ResB : MonoBehaviour
 {
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+    }
+
     public void OnClickStartButton()
     {
         if (Input.GetKeyDown(KeyCode.JoystickButton0) || (Input.GetKeyDown(KeyCode.Space)))
