6aa73adf578c79e2b756962ecf3687dd5dce2348
尾身洋亮 <no.color11095663@gmail.com>
2021-04-09 15:34:34 +0900

    Merge branch 'master' of https://github.com/estrela530/MNG46withB

---
+++ Assets/ClearStageUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearStageUI : MonoBehaviour
+{
+    public Text clearStageText;
+
+    //StageMoveのスクリプト取得
+    SceneGR sGR;
+    [SerializeField]
+    private GameObject sGRP;
+    int sText = 0;
+
+    //今は仮段階、　後にはこのstageNumberにクリアしたシーンから受け取るシーン番号を代入できるようにする
+    int stageNumber = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clearStageText = GetComponent<Text>();
+        sGR = sGRP.GetComponent<SceneGR>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //なんでfalseなん？
+        if (!sGR.changeOKFlag)
+        {
+            Debug.Log(sGR.changeOKFlag);
+            clearStageText.text = "Stage" + stageNumber;
+            //    clearStageText.text = "Stage";//+ ":" + stageNumber.ToString("1")
+            //    //sGR.changeOKFlag = false;
+        }
+    }
+}
---
+++ Assets/MissionManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionManagerScript : MonoBehaviour
+{
+    [SerializeField, Header("ミッション1星")]
+    GameObject Mission1;
+    [SerializeField, Header("ミッション2星")]
+    GameObject Mission2;
+    [SerializeField, Header("ミッション3星")]
+    GameObject Mission3;
+
+    GameObject m1;
+    GameObject m2;
+    GameObject m3;
+
+    float fadeCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fadeCount++;
+
+        if (fadeCount >= 120)
+        {
+            Mission1.GetComponent<Image>().color = Color.yellow;
+        }
+        if (fadeCount >= 300)
+        {
+            Mission2.GetComponent<Image>().color = Color.yellow;
+        }
+        if (fadeCount >= 480)
+        {
+            Mission3.GetComponent<Image>().color = Color.yellow;
+        }       
+    }
+}
