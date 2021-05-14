ec450e5c8ff665a7a95780848801a2ed7143df21
blue666225 <fwantazi1145@gmail.com>
2021-05-14 14:28:08 +0900

    オクタンボスのHPの出現、１ステージのボス修正した(召喚と打つだけにした)

--- Assets/Script/Enemys/Boss/BossMove.cs
+++ Assets/Script/Enemys/Boss/BossMove.cs
@@ -19,36 +19,16 @@ public class BossMove : MonoBehaviour
 
     [SerializeField, Header("体力")]
     float enemyHP = 5;
-
-    [SerializeField, Header("この数値以下になったら変わる")]
-    float ChangeAttackHP;
-
     
-    //[SerializeField, Header("発見時のスピード")]
-    //float speedLoc;
-
-    [SerializeField, Header("突進時のスピード")]
-    float RushSpeed;
     
     [Header("追う時のフラグ")]
      public bool MoveFlag = true;//追う
 
-    [Header("突進のフラグ")]
-     public bool RushFlag = false;//突進
-
-    [SerializeField, Header("何秒止まるか")]
-     float freezeTime;
-
-     private float lookTime;
-
-    [Header("何秒に実行するか")]
-    float RushRunTime;
-
-    [SerializeField,Header("突進のインターバル")]
-    float RushIntarval;
-
+    [Header("攻撃のフラグ")]
+     public bool AttackFlag;//
+    
     [SerializeField, Header("召喚するオブジェクト")]
-    GameObject PawnEnemy;
+    GameObject SummonEnemy;
 
     [SerializeField, Header("次の生成時間")]
     float ResetTime;
@@ -57,10 +37,15 @@ public class BossMove : MonoBehaviour
     float PawnTime;
 
     [SerializeField, Header("エネミーの出現数")]
-    int PawnCount;//プレハブの出現数
+    int EnemyCount;//プレハブの出現数
 
     [SerializeField, Header("召喚するエネミーの上限")]
-    int MaxPawnCount;//プレハブの出現数
+    int MaxEnemyCount;//プレハブの出現数
+
+    [SerializeField, Header("ボスショット")]
+    GameObject bossShot;
+
+    public int moveState;
 
     bool PawnFalg;
 
@@ -68,10 +53,17 @@ public class BossMove : MonoBehaviour
     [SerializeField, Header("次のしーんに行くの開始までの時間")]  float NextTime;
     [SerializeField,Header("次のシーンに行くフラグ")] bool NextFlag;
 
+    [SerializeField, Header("死んだ時のエフェクト")]
+    private GameObject DeathEffect;
+    private ParticleSystem DeathParticle;   //ダメージのパーティクル
+
+    int nextState = 0;
+
     //レイ関連
     Ray ray;
     RaycastHit hitRay;
     LineRenderer lineRenderer;
+    int enemyNumber = (1 << 13);
 
     Renderer renderComponent;
     [SerializeField] float ColorInterval = 0.1f;
@@ -79,14 +71,23 @@ public class BossMove : MonoBehaviour
     [SerializeField, Header("ダメージ受けた時")]
     bool DamageFlag = false;
 
+
+
     void Start()
     {
+        DeathParticle = DeathEffect.GetComponent<ParticleSystem>();
+
+        moveState = 0;
+        bossShot.GetComponent<BossShot>();
         PawnFalg = false;
         //Target = GameObject.Find("Player");//追尾させたいオブジェクトを書く
         Target = GameObject.FindGameObjectWithTag("Player");
         rigid = GetComponent<Rigidbody>();
         MoveFlag = true;
         NextFlag = false;
+        AttackFlag = true;
+
+        renderComponent = GetComponent<Renderer>();
 
         ray = new Ray();
         lineRenderer = this.gameObject.GetComponent<LineRenderer>();
@@ -144,26 +145,13 @@ public class BossMove : MonoBehaviour
 
         if (MoveFlag)
         {
-
-            //this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));//ターゲットにむく
-            //召喚
-            //if (enemyHP <= ChangeAttackHP)
-            //{
-            //    Pawn();
-            //}
-
-            dis = Vector3.Distance(transform.position, Target.transform.position);//二つの距離を計算して一定以下になれば追尾
-
-            lineRenderer.SetPosition(0, this.transform.position);
-
-          
             //レイの処理
-            if (Physics.Raycast(ray, out hitRay, 20))
+            if (Physics.Raycast(ray, out hitRay, 20, enemyNumber))
             {
                 if (hitRay.collider.gameObject.CompareTag("Player"))
                 {
                     lineRenderer.enabled = true;
-                    
+
                     lineRenderer.SetPosition(1, hitRay.point);
 
                 }
@@ -176,76 +164,129 @@ public class BossMove : MonoBehaviour
             ray.origin = this.transform.position;//自分の位置のレイ
 
             ray.direction = transform.forward;//自分の向きのレイ
-            
-            ////追いかける
-            //if (MoveFlag)
-            //{
-            //    this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));//ターゲットにむく
-            //}
 
-            //突進を実行するまで時間を足す
-            RushRunTime += Time.deltaTime;
+            //ターゲットにむく
+            this.transform.LookAt(
+                       new Vector3(Target.transform.position.x,
+                       this.transform.position.y,
+                       Target.transform.position.z));
 
-            if (RushRunTime >= RushIntarval)
+            switch (moveState)
             {
-                RushFlag = true;
-                RushRunTime = 0;
-            }
-            //突進の処理
-            if (RushFlag)
-            {
-                MoveFlag = false;
-                lookTime += Time.deltaTime;
+                //撃つ、召喚どっちか? 1～2が撃つ、3～4が召喚
+                case 0:
+                    if (bossShot.GetComponent<BossShot>().shotCount >= 3)
+                    {
+                        moveState = 3;//召喚
+                    }
+                    else if (bossShot.GetComponent<BossShot>().shotCount < 3)
+                    {
+                        moveState = 1;//
+                    }
+                    break;
 
-                //見つめてる
-                if (lookTime <= freezeTime)
-                {
-                    this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));//ターゲットにむく
-                }
-                //突進する
-                if (lookTime >= freezeTime)
-                {
-                    transform.position += transform.forward * RushSpeed * Time.deltaTime;//前進(スピードが変わる)
-                }
+                //見てるて撃つ
+                case 1:
+                    AttackFlag = true;
+                   
+
+                    lineRenderer.startColor = Color.red;//初めの色
+                    lineRenderer.endColor = Color.red;//終わりの色
+
+                    moveState = 2;
+
+                    break;
+                    
+                //戻す
+                case 2:
+                    
+                    moveState = 0;
+
+                    break;
+                    
+                case 3:
+                    if (EnemyCount == MaxEnemyCount)
+                    {
+                        moveState = 4;
+                    }
+                    //カウントの値まで生成
+                    if (EnemyCount < MaxEnemyCount)
+                    {
+                        PawnTime -= Time.deltaTime;
+                        if (PawnTime <= 0.0f)
+                        {
+                            PawnTime = ResetTime;//1秒沖に生成
+                            var sum = Instantiate(SummonEnemy,
+                                new Vector3(transform.position.x + 2, transform.position.y, transform.position.z + 2),
+                                Quaternion.identity);
+
+                            EnemyCount++;
+                        }
+                        
+                    }
+                    break;
+
+                //戻す
+                case 4:
+                    moveState = 0;
+                    EnemyCount = 0;
+                    bossShot.GetComponent<BossShot>().shotCount = 0;
+                    break;
+                    
+
             }
+            //this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));//ターゲットにむく
+           
+            dis = Vector3.Distance(transform.position, Target.transform.position);//二つの距離を計算して一定以下になれば追尾
+
+            lineRenderer.SetPosition(0, this.transform.position);
         }
-        if (enemyHP <= 0)
-        {
-            MoveFlag = false;
-            DeathTime += Time.deltaTime;
-            if(DeathTime > NextTime)
-            {
-                NextFlag = true;
-                DeathTime = 0;
-            }
-        }
 
-        if(NextFlag)
+        switch (nextState)
         {
-            gameObject.SetActive(false);//非表示
-            //SceneManager.LoadScene("Result");
-            SceneManager.LoadScene("GameClear");
+            case 0:
+                if (enemyHP <= 0)
+                {
+                    nextState = 1;
+                }
+                break;
+
+            case 1:
+
+                var sum = Instantiate(DeathEffect,
+                           this.transform.position,
+                           Quaternion.identity);
+
+                nextState = 2;
+
+                bossShot.GetComponent<BossShot>().shotCount = 0;
+                moveState = 0;
+                EnemyCount = 5;
+
+                break;
+
+            case 2:
+                DeathTime += Time.deltaTime;
+                if (DeathTime > NextTime)
+                {
+
+                    DeathTime = 0;
+
+                    nextState = 3;
+                }
+
+
+                break;
+
+            case 3:
+                SceneManager.LoadScene("GameClear");
+                gameObject.SetActive(false);//非表示
+                break;
+
         }
        
     }
-
-    //召喚する処理
-    void Pawn()
-    {
-        //カウントの値まで生成
-        if (PawnCount<MaxPawnCount)
-        {
-            PawnTime -= Time.deltaTime;
-            if(PawnTime <= 0.0f)
-            {
-                PawnTime = ResetTime;//1秒沖に生成
-                GameObject.Instantiate(PawnEnemy);
-                //GameObject.Instantiate(PawnEnemy);
-                PawnCount++;
-            }
-        }
-    }
-
+    
 
     public int HpGet()
     {
@@ -264,9 +305,7 @@ public class BossMove : MonoBehaviour
 
         if (other.gameObject.CompareTag("Player")|| (other.gameObject.CompareTag("Wall")))
         {
-            lookTime = 0;
             MoveFlag = true;
-            RushFlag = false;
         }
 
     }
