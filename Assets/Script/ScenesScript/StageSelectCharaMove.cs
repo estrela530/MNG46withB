48cbcfa58e7708a18dcd1a5141861cdb588ba075
estrela530 <daisuke.0530.otk@gmail.com>
2021-05-25 05:02:15 +0900

    StageSelect（微妙）とフェードの見た目を変えてみました

--- Assets/Script/ScenesScript/StageSelectCharaMove.cs
+++ Assets/Script/ScenesScript/StageSelectCharaMove.cs
@@ -21,19 +21,12 @@ public class StageSelectCharaMove : MonoBehaviour
     [SerializeField, Header("ステージ6")]
     GameObject s6Image;
 
-    [SerializeField, Header("ボタン1")]
-    GameObject s1Button;
-    [SerializeField, Header("ボタン2")]
-    GameObject s2Button;
-    [SerializeField, Header("ボタン3")]
-    GameObject s3Button;
-    [SerializeField, Header("ボタン4")]
-    GameObject s4Button;
-    [SerializeField, Header("ボタン5")]
-    GameObject s5Button;
-    [SerializeField, Header("ボタン6")]
-    GameObject s6Button;
-
+    [SerializeField, Header("ボタン1・2キャンバス")]
+    GameObject s12Canvas;
+    [SerializeField, Header("ボタン3・4キャンバス")]
+    GameObject s34Canvas;
+    [SerializeField, Header("ボタン5・6キャンバス")]
+    GameObject s56Canvas;
 
     [SerializeField, Header("シーン遷移")]
     GameObject sceneSGObj;
@@ -48,15 +41,17 @@ public class StageSelectCharaMove : MonoBehaviour
     public bool button5NowFlag = false;
     public bool button6NowFlag = false;
 
-    Vector3 nejirinnStage1Pos = new Vector3(-40, -23, 80);
-    Vector3 nejirinnStage2Pos = new Vector3(5, -9, 80);
+    Vector3 nejirinStage1Pos = new Vector3(-40, -23, 80);
+    Vector3 nejirinStage2Pos = new Vector3(5, -9, 80);
 
     public bool isMissionImageFlag;
+    int moveCount;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        nejirin.transform.position = nejirinnStage1Pos;
+        nejirin.transform.position = nejirinStage1Pos;
         isMissionImageFlag = true;
         button1NowFlag = true;
         button2NowFlag = false;
@@ -66,147 +61,154 @@ public class StageSelectCharaMove : MonoBehaviour
         button6NowFlag = false;
         s2Image.SetActive(false);
         s1Image.SetActive(true);
-        s1Button.SetActive(true);
-        s2Button.SetActive(true);
-        s3Button.SetActive(false);
-        s4Button.SetActive(false);
-        s5Button.SetActive(false);
-        s6Button.SetActive(false);
+        s12Canvas.SetActive(true);
+        s34Canvas.SetActive(false);
+        s56Canvas.SetActive(false);
 
+        moveCount = 0;
         sceneSG = sceneSGObj.GetComponent<SceneSG>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //moveCount++;
         //L Stick
         float lsh = Input.GetAxisRaw("Horizontal");
-
         isSceneMoveFlag = sceneSG.GetIsMoveFlag();
-
+        Debug.Log("呼ばれた！");
         //1->2
-        if (Input.GetKeyDown(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button1NowFlag)
+        if (Input.GetKeyUp(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button1NowFlag)
         {
-            Debug.Log("右押された");
+            Debug.Log("1で右押された");
             button1NowFlag = false;
             button2NowFlag = true;
             s1Image.SetActive(false);
             s2Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage2Pos;
+            nejirin.transform.position = nejirinStage2Pos;
             isMissionImageFlag = true;
         }
         //2->1
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button2NowFlag)
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button2NowFlag)
         {
-            Debug.Log("左押された");
+            Debug.Log("2で左押された");
             button2NowFlag = false;
             button1NowFlag = true;
             s2Image.SetActive(false);
             s1Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            nejirin.transform.position = nejirinStage1Pos;
             isMissionImageFlag = true;
         }
         //2->3
-        if (Input.GetKeyDown(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button2NowFlag)
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button2NowFlag)
         {
-            Debug.Log("右押された");
+            Debug.Log("2で右押された");
             button2NowFlag = false;
             button3NowFlag = true;
             s2Image.SetActive(false);
             s3Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage2Pos;
+            s12Canvas.SetActive(false);
+            s34Canvas.SetActive(true);
+            nejirin.transform.position = nejirinStage2Pos;
             isMissionImageFlag = true;
         }
         //3->2
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button3NowFlag)
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button3NowFlag)
         {
-            Debug.Log("左押された");
+            Debug.Log("3で左押された");
             button3NowFlag = false;
             button2NowFlag = true;
             s3Image.SetActive(false);
             s2Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            s34Canvas.SetActive(false);
+            s12Canvas.SetActive(true);
+            nejirin.transform.position = nejirinStage1Pos;
             isMissionImageFlag = true;
         }
         //3->4
-        if (Input.GetKeyDown(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button3NowFlag)
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button3NowFlag)
         {
-            Debug.Log("右押された");
+            Debug.Log("3で右押された");
             button3NowFlag = false;
             button4NowFlag = true;
             s3Image.SetActive(false);
             s4Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage2Pos;
+            nejirin.transform.position = nejirinStage2Pos;
             isMissionImageFlag = true;
         }
         //4->3
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button4NowFlag)
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button4NowFlag)
         {
-            Debug.Log("左押された");
+            Debug.Log("4で左押された");
             button4NowFlag = false;
             button3NowFlag = true;
             s4Image.SetActive(false);
             s3Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            nejirin.transform.position = nejirinStage1Pos;
             isMissionImageFlag = true;
         }
         //4->5
-        if (Input.GetKeyDown(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button4NowFlag)
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button4NowFlag)
         {
-            Debug.Log("右押された");
+            Debug.Log("4で右押された");
             button4NowFlag = false;
             button5NowFlag = true;
             s4Image.SetActive(false);
             s5Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage2Pos;
+            s34Canvas.SetActive(false);
+            s56Canvas.SetActive(true);
+            nejirin.transform.position = nejirinStage2Pos;
             isMissionImageFlag = true;
         }
         //5->4
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button5NowFlag)
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button5NowFlag)
         {
-            Debug.Log("左押された");
+            Debug.Log("5で左押された");
             button5NowFlag = false;
             button4NowFlag = true;
             s5Image.SetActive(false);
             s4Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            s56Canvas.SetActive(false);
+            s34Canvas.SetActive(true);
+            nejirin.transform.position = nejirinStage1Pos;
             isMissionImageFlag = true;
         }
         //5->6
-        if (Input.GetKeyDown(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button5NowFlag)
+        else if (Input.GetKeyUp(KeyCode.RightArrow) || lsh > 0 && !isSceneMoveFlag && button5NowFlag)
         {
-            Debug.Log("右押された");
+            Debug.Log("5で右押された");
             button5NowFlag = false;
             button6NowFlag = true;
             s5Image.SetActive(false);
             s6Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage2Pos;
+            nejirin.transform.position = nejirinStage2Pos;
             isMissionImageFlag = true;
         }
         //6->5
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button6NowFlag)
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || lsh < 0 && !isSceneMoveFlag && button6NowFlag)
         {
-            Debug.Log("左押された");
+            Debug.Log("6で左押された");
             button6NowFlag = false;
             button5NowFlag = true;
             s6Image.SetActive(false);
             s5Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            nejirin.transform.position = nejirinStage1Pos;
             isMissionImageFlag = true;
         }
         //クリックしたらButton1へ
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        else if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
         {
             button1NowFlag = true;
-            s2Image.SetActive(false);
-            s3Image.SetActive(false);
-            s4Image.SetActive(false);
-            s5Image.SetActive(false);
-            s6Image.SetActive(false);
-            s1Image.SetActive(true);
-            nejirin.transform.position = nejirinnStage1Pos;
+            s12Canvas.SetActive(true);
+            s34Canvas.SetActive(false);
+            s56Canvas.SetActive(false);
+            s34Canvas.SetActive(false);
+            s56Canvas.SetActive(false);
+            s12Canvas.SetActive(true);
+            nejirin.transform.position = nejirinStage1Pos;
             button1NowFlag = false;
             isMissionImageFlag = true;
         }
+
     }
 }
