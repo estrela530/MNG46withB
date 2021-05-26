ecc7e13e08e7c0b754f05af6ca09c5894981961a
blue666225 <fwantazi1145@gmail.com>
2021-05-27 01:32:02 +0900

    （亀、亀の弾、サソリ、サソリ(ミニ)、サソリの弾、サボテン、サボテンの弾）のモデルの軸修正ボスが死んだ時のカメラの演出、エネミーのダメージ受けた時の色変え

---
+++ Assets/Script/Enemys/Foundation/Damage.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    bool flag;
+
+    //[SerializeField] BossMove enemy;
+    [Header("アタッチしたエネミー")]
+    public GameObject Enemy;
+
+    private EnemyMove Normal;
+    private OctaneEnemy Octane;
+    private OctaneWand OctaneWand;
+    private KraberEnemy Kraber;
+    private PoisonEnemy Poison;
+    private BossMove Boss;
+    private PawnEnemy Pawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //オクタン
+        if (this.Enemy.GetComponent<OctaneEnemy>())
+        {
+            Octane = this.Enemy.GetComponent<OctaneEnemy>();
+        }
+
+        //
+        if (this.Enemy.GetComponent<KraberEnemy>())
+        {
+            Kraber = this.Enemy.GetComponent<KraberEnemy>();
+        }
+
+        //ノーマル
+        if (this.Enemy.GetComponent<EnemyMove>())
+        {
+            Normal = this.Enemy.GetComponent<EnemyMove>();
+            flag = Normal.DamageGet();
+        }
+
+        //ポイズン
+        if (this.Enemy.GetComponent<PoisonEnemy>())
+        {
+            Poison = this.Enemy.GetComponent<PoisonEnemy>();
+            
+        }
+
+        //ボス
+        if (this.Enemy.GetComponent<BossMove>())
+        {
+            Boss = this.Enemy.GetComponent<BossMove>();
+        }
+
+        //ザコ
+        if (this.Enemy.GetComponent<PawnEnemy>())
+        {
+            Pawn = this.Enemy.GetComponent<PawnEnemy>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+        //オクタン
+        if (this.Enemy.GetComponent<OctaneEnemy>())
+        {
+            
+        }
+
+        //クレーバー
+        else if (this.Enemy.GetComponent<KraberEnemy>())
+        {
+           
+        }
+
+        //ノーマル
+        else if (this.Enemy.GetComponent<EnemyMove>())
+        {
+            flag = Normal.DamageGet();
+        }
+
+        //ポイズン
+        else if (this.Enemy.GetComponent<PoisonEnemy>())
+        {
+           
+        }
+
+        //ボス
+        else if (this.Enemy.GetComponent<BossMove>())
+        {
+           
+        }
+
+        //ザコ
+        else if (this.Enemy.GetComponent<PawnEnemy>())
+        {
+            
+        }
+
+        if (flag)
+        {
+            Eff();
+        }
+
+    }
+
+    void Eff()
+    {
+        float level = Mathf.Abs(Mathf.Sin(Time.time * 5));
+        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f, level);
+        StartCoroutine("WaitForIt");
+    }
+
+    IEnumerator WaitForIt()
+    {
+        // 1秒間処理を止める
+        yield return new WaitForSeconds(1);
+
+        // １秒後ダメージフラグをfalseにして点滅を戻す
+        flag = false;
+        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 1f);
+    }
+}
---
+++ Assets/Script/SubCameraControll.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubCameraControll : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject subCamera1;
+    [SerializeField] GameObject subCamera2;
+    [SerializeField] GameObject subCamera3;
+    [SerializeField] GameObject Boss;
+    private BossMove BossEnemy;
+    private OctaneEnemy Octane;
+    float ObjHp;
+    [SerializeField] float speedLoc = 5;
+    [SerializeField] int cameraState;
+    [SerializeField] float transitionTime1;
+    [SerializeField] float transitionTime2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mainCamera = GameObject.Find("Main Camera");
+        //subCamera = GameObject.Find("SubCamera");
+
+        //オクタン
+        if (this.Boss.GetComponent<OctaneEnemy>())
+        {
+            Octane = this.Boss.GetComponent<OctaneEnemy>();
+            ObjHp = Octane.HpGet();
+        }
+        //ボス
+        if (this.Boss.GetComponent<BossMove>())
+        {
+            BossEnemy = this.Boss.GetComponent<BossMove>();
+            ObjHp = BossEnemy.HpGet();
+        }
+        mainCamera.SetActive(true);
+        cameraState = 0;
+        subCamera1.SetActive(false);
+        subCamera2.SetActive(false);
+        subCamera3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //召喚
+        if (ObjHp <= 0)
+        {
+            CameraMove();
+        }
+
+        //オクタン
+        if (this.Boss.GetComponent<OctaneEnemy>())
+        {
+            ObjHp = Octane.HpGet();
+        }
+        //ボス
+        else if (this.Boss.GetComponent<BossMove>())
+        {
+            ObjHp = BossEnemy.HpGet();
+        }
+
+
+    }
+
+    void CameraMove()
+    {
+        mainCamera.SetActive(false);
+        switch (cameraState)
+        {
+            case 0:
+                cameraState = 1;
+                break;
+
+            case 1:
+                subCamera1.SetActive(true);
+                transitionTime1 -= Time.deltaTime;
+
+                subCamera1.transform.LookAt(Boss.transform.position);
+                subCamera1.transform.position += transform.forward * speedLoc * Time.deltaTime;
+
+                if(transitionTime1<=0)
+                {
+                    cameraState = 2;
+                }
+                
+                break;
+
+            case 2:
+                transitionTime2 -= Time.deltaTime;
+                subCamera1.SetActive(false);
+                subCamera2.SetActive(true);
+
+                subCamera2.transform.LookAt(Boss.transform.position);
+                subCamera2.transform.position += transform.forward * speedLoc * Time.deltaTime;
+                if (transitionTime2 <= 0)
+                {
+                    cameraState = 3;
+                }
+
+                break;
+
+            case 3:
+                subCamera2.SetActive(false);
+                subCamera3.SetActive(true);
+
+                subCamera3.transform.LookAt(Boss.transform.position);
+                subCamera3.transform.position += transform.forward * speedLoc * Time.deltaTime;
+                break;
+        }
+       
+    }
+}
