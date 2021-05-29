6a220012fc8446399d2f0cd3b41b7beda1caf2ff
estrela530 <daisuke.0530.otk@gmail.com>
2021-05-30 03:56:54 +0900

    プロローグの完成と、スキップの作成

---
+++ Assets/SkiptoScene.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkiptoScene : MonoBehaviour
+{
+    int skipCount;
+    bool isPush;
+    [SerializeField, Header("長押しで行きたいシーン名")]
+    string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skipCount = 0;
+        isPush = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("joystick button 4") || Input.GetKey(KeyCode.Z))
+        {
+            PushButton();
+        }
+        else
+        {
+            UpButton();
+            skipCount = 0;
+        }
+
+        if (isPush)
+        {
+            skipCount++;
+            if (skipCount >= 120)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+        else
+        {
+            if (skipCount >= 180)
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+
+
+    }
+
+    public void PushButton()
+    {
+        isPush = true;
+    }
+
+    public void UpButton()
+    {
+        isPush = false;
+    }
+
+}
---
+++ Assets/VideoSkip.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoSkip : MonoBehaviour
+{
+    public VideoClip videoClip;
+    public GameObject screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var videoPlayer = screen.GetComponent<VideoPlayer>();
+
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videoClip;
+
+        videoPlayer.isLooping = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VPControl();
+    }
+
+    void VPControl()
+    {
+        var videoPlayer = GetComponent<VideoPlayer>();
+
+        if (!videoPlayer.isPlaying)
+        {
+            videoPlayer.loopPointReached += FinishVideo;
+            videoPlayer.Play();
+        }
+    }
+
+    void FinishVideo(VideoPlayer videoPlayer)
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+}
