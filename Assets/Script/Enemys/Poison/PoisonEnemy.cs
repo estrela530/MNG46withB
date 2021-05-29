701410dc848dc2615225794cf2ee34af928869ab
blue666225 <fwantazi1145@gmail.com>
2021-05-30 08:45:48 +0900

    全て(指定した)のエネミーに死亡アニメーションモドキを追加

--- Assets/Script/Enemys/Poison/PoisonEnemy.cs
+++ Assets/Script/Enemys/Poison/PoisonEnemy.cs
@@ -41,8 +41,21 @@ public class PoisonEnemy : MonoBehaviour
     bool DamageFlag = false;
 
     [SerializeField, Header("死んだ時のエフェクト")]
-    private GameObject DeathEffect;
+    private GameObject deathEffect;
     private ParticleSystem DeathParticle;   //ダメージのパーティクル
+
+    //追加
+    private int deathState;//死亡状態
+    private bool isDeadFlag = false;//死んでいるか？
+    [SerializeField, Tooltip("オブジェクトが飛んでいく力")]
+    private float jumpPower = 18.0f;
+    [SerializeField, Tooltip("オブジェクトの最大到達地点")]
+    private float topHeightPoint = 5;
+    private float deathTime = 0;
+    [SerializeField, Tooltip("死亡エフェクトがでるまでの時間")]
+    private float deathEffectTime = 1.0f;
+    private int childCount;//子どもの数
+    private GameObject[] child;
 
     GameObject stageMove1;
 
@@ -50,7 +63,7 @@ public class PoisonEnemy : MonoBehaviour
     void Start()
     {
         MaxEnemyHP = enemyHP;
-        DeathParticle = DeathEffect.GetComponent<ParticleSystem>();
+        DeathParticle = deathEffect.GetComponent<ParticleSystem>();
         //Target = GameObject.Find("Player");//追尾させたいオブジェクトを書く
         Target = GameObject.FindGameObjectWithTag("Player");
         rigid = GetComponent<Rigidbody>();
@@ -73,6 +86,18 @@ public class PoisonEnemy : MonoBehaviour
         lineRenderer.endWidth = 0.5f;
         //変えるかも?
         ray.direction = transform.forward;
+
+        //子どもの数を取得
+        childCount = gameObject.transform.childCount;
+        //配列を子どもオブジェクトの数で初期化
+        child = new GameObject[childCount];
+        //オブジェクトを代入していく
+        for (int i = 0; i < childCount; i++)
+        {
+            child[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+        deathState = 0;
+        isDeadFlag = false;
     }
 
     //中断できる処理のまとまり
@@ -89,6 +114,9 @@ public class PoisonEnemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        DeathAction();
+
+        if (isDeadFlag) return;
 
         rigid.angularVelocity = Vector3.zero;
         rigid.velocity = Vector3.zero;
@@ -119,34 +147,18 @@ public class PoisonEnemy : MonoBehaviour
         //常にターゲットにむく
         this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));
 
+       
+
         if (stageMove1.GetComponent<StageMove1>().nowFlag == true)
         {
             MoveFlag = false;
         }
 
-        if (enemyHP <= 0 && !stageMove1.GetComponent<StageMove1>().bossNow)
-        {
-            Destroy(this.gameObject);
-            //gameObject.SetActive(false);//非表示
-            TimerScript.enemyCounter += 1;
-            var sum = Instantiate(DeathEffect,
-                          this.transform.position,
-                          Quaternion.identity);
-
-        }
+        
 
         dis = Vector3.Distance(transform.position, Target.transform.position);//二つの距離を計算して一定以下になれば追尾
         
-        //if (dis < area)
-        //{
-        //    MoveFlag = true;
-        //    workFlag = false;
-        //}
-        //else if(dis>area)
-        //{
-        //    MoveFlag = false;
-        //    workFlag = true;
-        //}
+        
         lineRenderer.SetPosition(0, this.transform.position);
 
         if (!Physics.Raycast(ray, out hitRay, 20,enemyNumber))
@@ -176,54 +188,68 @@ public class PoisonEnemy : MonoBehaviour
         ray.origin = this.transform.position;//自分の位置のレイ
 
         ray.direction = transform.forward;//自分の向きのレイ
-
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 0.1f);
-
-        //if (MoveFlag)
-        //{
-        //    this.transform.LookAt(new Vector3(Target.transform.position.x, this.transform.position.y, Target.transform.position.z));//ターゲットにむく
-        //    //if (dis >= social)
-        //    //{
-        //    //    transform.position += transform.forward * speedLoc * Time.deltaTime;//前進(スピードが変わる)
-        //    //}
-
-        //}
-
-        //徘徊
-        //if (workFlag)
-        //{
-        //    if (ww < workeAria1)
-        //    {
-        //        workNumber = 2;
-        //    }
-        //    if (ww2 < workeAria2)
-        //    {
-        //        workNumber = 1;
-        //    }
-
-        //    switch (workNumber)
-        //    {
-
-        //        case 1:
-
-        //            this.transform.LookAt(this.workObj1.transform);//徘徊1の位置に向く
-        //            transform.position += transform.forward * speed * Time.deltaTime;
-
-        //            break;
-
-        //        case 2:
-
-        //            this.transform.LookAt(this.workObj2.transform);//徘徊2の位置に向く
-        //            transform.position += transform.forward * speed * Time.deltaTime;
-
-        //            break;
-        //    }
-
-
-        //}
+        
 
     }
+    void DeathAction()
+    {
+        switch (deathState)
+        {
+            case 0:
+                //体力がなくなったら死亡&状態遷移
+                if (enemyHP <= 0 && !stageMove1.GetComponent<StageMove1>().bossNow)
+                {
+                    deathState = 1;
+                    isDeadFlag = true;
+                    DamageFlag = true;
+                }
+                break;
 
+            case 1:
+                //Y軸にも動けるようにした後、上に移動する
+                rigid.constraints = RigidbodyConstraints.None;
+                rigid.constraints = RigidbodyConstraints.FreezeRotation;
+                rigid.AddForce(Vector3.up * jumpPower);
+
+                if (this.transform.position.y > topHeightPoint)
+                {
+                    this.transform.position = new Vector3(this.transform.position.x, topHeightPoint, this.transform.position.z);
+                }
+
+                //一定時間経過後、状態遷移
+                deathTime += Time.deltaTime;
+                if (deathTime > deathEffectTime)
+                {
+                    deathTime = 0;
+                    deathState = 2;
+                }
+                break;
+
+            case 2:
+                //自身と、自身の子どもを非表示にする
+                for (int i = 0; i < childCount; i++)
+                {
+                    child[i] = gameObject.transform.GetChild(i).gameObject;
+                    child[i].SetActive(false);
+                }
+
+                //パーティクルオブジェクトを生成
+                var sum = Instantiate(deathEffect, this.transform.position, Quaternion.identity);
+
+                //状態遷移
+                deathState = 3;
+                break;
+
+            case 3:
+                //死亡した
+                if (!stageMove1.GetComponent<StageMove1>().bossNow)
+                {
+                    TimerScript.enemyCounter += 1;
+                }
+                Destroy(this.gameObject);
+                break;
+        }
+    }
     public float HpGet()
     {
         return enemyHP;
