4dedbad96f8b904b070e1f8e36727c99eaa73d6e
kubota_syou <kirby8250@gmail.com>
2021-05-31 02:52:07 +0900

    5/31_エピローグの画像変更&BGM追加

--- Assets/FungusController.cs
+++ Assets/FungusController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class FungusController
+public class FungusController : MonoBehaviour
 {
     void LoadScene()
     {
